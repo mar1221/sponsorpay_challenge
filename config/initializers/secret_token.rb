2f82947cac47bb3518ee19514dd086afbd6604f4
Marek Takáč <takac.webdevelopment@gmail.com>
2014-03-21 10:17:00 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SponsorpayChallenge::Application.config.secret_key_base = '8992761043849c32b3f8859e8e00091b83ff490a572e6a43a0227e621c10337e6dcc045219c13038b660939d27e748d2c378850662f0551a224f673015e75ed4'
