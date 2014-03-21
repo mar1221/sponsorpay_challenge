146ef813b877a70a75a1cec539249e16fc80c914
Marek Takáč <takac.webdevelopment@gmail.com>
2014-03-21 15:45:15 +0100

    modified README

--- app/models/request.rb
+++ app/models/request.rb
@@ -6,5 +6,9 @@ class Request
   attr_accessor(:uid, :pub0, :page)
 
   validates :uid, presence: true
-  validates :page, presence: true, format: { with: /^\d*$/, multiline: true, message: 'must be a positive integer.' }
+  validates :page, presence: true, format: {
+    with: /^\d*$/,
+    multiline: true,
+    message: 'must be a positive integer.'
+  }
 end
