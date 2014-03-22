b60678d568eb75192c532eac0fdf924c94074a23
Marek Takáč <takac.webdevelopment@gmail.com>
2014-03-22 15:55:35 +0100

    added and refactored tests in offers controller spec

--- spec/controllers/offers_controller_spec.rb
+++ spec/controllers/offers_controller_spec.rb
@@ -3,59 +3,54 @@ require "spec_helper"
 describe OffersController do
   describe "GET #index" do
 
-    context "HTTP request" do
-      it "responds with an HTTP 200 status code" do
-        get :index
-        expect(response).to be_success
-        expect(response.status).to eq(200)
-      end
+    subject { response }
 
-      it "renders the index template" do
-        get :index
-        expect(response).to render_template("index")
-      end
+    context "HTTP request" do
+      before { get :index }
+
+      it { should be_success }
+      it { should render_template('index') }
     end
 
     context "json request" do
+      before do
+        matching_uri = VCR.request_matchers.uri_without_params(:timestamp, :hashkey)
+        VCR.use_cassette('valid_response', match_requests_on: [:method, matching_uri]) do
+          get :index, uid: 1, page: 1, format: :json
+        end
+      end
+
       context "with valid required params" do
+        let!(:parsed_offers) { ActiveSupport::JSON.decode(response.body)['offers'] }
 
-        context 'with a response signature' do
+        it { should be_success }
+        it 'returns a list of offers' do
+            expect(parsed_offers).to eq([])
+        end
 
-          it "responds with an HTTP 200 status code" do
+        context "when requesting non-existent page" do
+          before do
             matching_uri = VCR.request_matchers.uri_without_params(:timestamp, :hashkey)
-            VCR.use_cassette('valid_response', match_requests_on: [:method, matching_uri]) do
-              get :index, uid: 1, page: 1, format: :json
-              expect(response.status).to eq(200)
+            VCR.use_cassette('nonexistent_page', match_requests_on: [:method, matching_uri]) do
+              get :index, uid: 1, page: 2, format: :json
             end
           end
+          let!(:parsed_code) { ActiveSupport::JSON.decode(response.body)['code'] }
 
-          it 'returns a list of offers' do
-            matching_uri = VCR.request_matchers.uri_without_params(:timestamp, :hashkey)
-            VCR.use_cassette('valid_response', match_requests_on: [:method, matching_uri]) do
-              get :index, uid: 1, page: 1, format: :json
-              parsed_response = ActiveSupport::JSON.decode(response.body)
-              expect(parsed_response['offers']).to eq([])
-            end
+          it 'returns appropriate error code' do
+            expect(parsed_code).to eq('ERROR_INVALID_PAGE')
           end
         end
       end
 
       context "with invalid params" do
-        it "responds with HTTP 422 status code" do
-          matching_uri = VCR.request_matchers.uri_without_params(:timestamp, :hashkey)
-          VCR.use_cassette('invalid_response', match_requests_on: [:method, matching_uri]) do
-            get :index, format: :json
-            expect(response.status).to eq(422)
-          end
-        end
+        before { get :index, format: :json }
+        let!(:response_body) { response.body }
+        let!(:expected_response) { { uid: ["can't be blank"], page: ["can't be blank"] }.to_json }
 
+        it { should_not be_success }
         it "returns a hash with errors" do
-          matching_uri = VCR.request_matchers.uri_without_params(:timestamp, :hashkey)
-          VCR.use_cassette('invalid_response', match_requests_on: [:method, matching_uri]) do
-            get :index, format: :json
-            expected_response = { uid: ["can't be blank"], page: ["can't be blank"] }.to_json
-            expect(response.body).to eq(expected_response)
-          end
+          expect(response_body).to eq(expected_response)
         end
       end
 
